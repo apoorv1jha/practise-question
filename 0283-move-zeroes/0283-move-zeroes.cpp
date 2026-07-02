fec3be6664cac1e77b54c76a79675de720519bed
apoorv1jha <jha77apurva@gmail.com>
2026-07-03 02:04:58 +0530

    Time: 0 ms (100%), Space: 23.9 MB (18.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,16 +3,24 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         vector<int>ans;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                ans.push_back(nums[i]);
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=0){
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<ans.size();i++){
+        //     nums[i]=ans[i];
+        // }
+        // for(int i=ans.size();i<n;i++){
+        //     nums[i]=0;
+        // }
+        int x =0;
+        int y =0;
+        for(int y=0;y<n;y++){
+            if(nums[y]!=0){
+                swap(nums[x],nums[y]);
+                x++;
             }
-        }
-        for(int i=0;i<ans.size();i++){
-            nums[i]=ans[i];
-        }
-        for(int i=ans.size();i<n;i++){
-            nums[i]=0;
         }
     }
 };

4817cd8ddef0cd6fc40f5b3be17ed1c47bcd2522
apoorv1jha <jha77apurva@gmail.com>
2026-07-04 23:25:13 +0530

    Time: 0 ms (100%), Space: 97.5 MB (24.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,9 +5,7 @@ public:
         int minp = INT_MAX;
         int maxp=0;
         for(int i=0;i<n;i++){
-           if(prices[i]<minp){
-            minp=prices[i];
-           }
+           minp = min(minp,prices[i]);
            maxp = max(maxp,prices[i]-minp);
         }
         return maxp;

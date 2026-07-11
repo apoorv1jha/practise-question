faea0ec553da36a5a8ac76bb2e55ceb66c5fad54
apoorv1jha <jha77apurva@gmail.com>
2026-07-11 23:00:17 +0530

    Time: 199 ms (42.19%), Space: 12.7 MB (64.79%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        vector<bool> iscomposite(n, false);
+
+        for (int i = 2; i * i < n; i++) {
+            if (!iscomposite[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    iscomposite[j] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!iscomposite[i]) count++;
+        }
+        return count;
+    }
+};

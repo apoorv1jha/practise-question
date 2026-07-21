e605c77e6c4ba535cf32d4fb0d576091ee5a52d2
apoorv1jha <jha77apurva@gmail.com>
2026-07-21 14:54:30 +0530

    Time: 0 ms (100%), Space: 19 MB (48.15%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -4,15 +4,25 @@ public:
        int n = pushed.size();
        int m = popped.size();
 
-       int  i=0,j=0;
-       for(int x=0;x<pushed.size();x++){
-        pushed[i++]=pushed[x];
+    //    int  i=0,j=0;
+    //    for(int x=0;x<pushed.size();x++){
+    //     pushed[i++]=pushed[x];
 
-        while(i-1>=0 &&j<m && pushed[i-1]==popped[j]){
-            i--;
+    //     while(i-1>=0 &&j<m && pushed[i-1]==popped[j]){
+    //         i--;
+    //         j++;
+    //     }
+    //    }
+    int j =0;
+    stack<int>st;
+    for(int i=0;i<n&&j<m;i++){
+        st.push(pushed[i]);
+        
+        while(!st.empty() && j<m && st.top()==popped[j]){
+            st.pop();
             j++;
         }
-       }
+    }
        return j==m;
     }
 };

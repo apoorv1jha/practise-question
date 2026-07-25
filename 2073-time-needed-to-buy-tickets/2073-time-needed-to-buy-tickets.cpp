e96fbbf2949c9797c088f90b8ea28ba40bb05576
apoorv1jha <jha77apurva@gmail.com>
2026-07-25 15:45:38 +0530

    Time: 0 ms (100%), Space: 10.3 MB (98.7%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n = tickets.size();
+        int count = 0;
+
+        while(tickets[k]>0){
+            for(int i=0;i<n;i++){
+                if(tickets[i]>0){
+                    tickets[i]--;
+                    count++;
+                }
+                if(tickets[k]==0) {
+                    return count;
+                    }
+            }
+        }
+        return count;
+    }
+};

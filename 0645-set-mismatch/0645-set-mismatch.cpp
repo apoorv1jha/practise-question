8faf058f9e5c7a8fbebbbe899ded1ada563d8e44
apoorv1jha <jha77apurva@gmail.com>
2026-07-25 14:51:14 +0530

    Time: 1494 ms (5.01%), Space: 25.2 MB (75.39%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -11,7 +11,7 @@ public:
                     count++;
                 }
             }
-            if(count ==2){
+            if(count >=2){
                 x =i; 
             }
             if(count ==0){

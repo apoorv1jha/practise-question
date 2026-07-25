4500c060c6f484520513223737f7732cfad75df6
apoorv1jha <jha77apurva@gmail.com>
2026-07-25 05:58:15 +0530

    Time: 2049 ms (36.2%), Space: 98.4 MB (23.37%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -4,6 +4,8 @@ public:
         unordered_set<int>pair;
         unordered_set<int>triplet;
         int n = nums.size();
+        // exp- 6^6 = 0 && 6^6^7=7
+        // pehle iske pair ka xor nikalo then iske pair k saath baaki number ka xor nikal lo
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 pair.insert(nums[i]^nums[j]);

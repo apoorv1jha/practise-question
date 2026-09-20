a205a2f29d638ac1aa46f4ca21139b0b806b8434
Apurva jha <jha77apurva@gmail.com>
2026-09-21 00:54:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (5.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n= nums.size();
+
+        //brute-force
         // for(int i=0;i<n;i++){
         // int count =0;
         //     for(int j=0;j<n;j++){
@@ -14,13 +16,41 @@ public:
         //     }
         // }
         //     return 0;
-        unordered_map<int,int>mp;
-        for(int num:nums){
-            mp[num]++;
+
+        // hashing
+        // unordered_map<int,int>mp;
+        // for(int num:nums){
+        //     mp[num]++;
+        // }
+        // for(auto & it:mp){
+        //     if(it.second>n/2)
+        //     return it.first;
+        // }
+        // return -1;
+
+        // boyer-moore problem
+        int count =0;
+        int elm ;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                count =1;
+                elm = nums[i];
+            }
+            else if(elm == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-        for(auto & it:mp){
-            if(it.second>n/2)
-            return it.first;
+        int count1=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==elm){
+                count1++;
+            }
+        }
+        if(count1>n/2){
+            return elm;
         }
         return -1;
     }
